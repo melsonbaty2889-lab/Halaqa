6ca210414406781a5123f89e864d70ba9f634128
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-05-28 12:51:53 +0300

    Update supabaseClient.js

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "YOUR_SUPABASE_URL",
-  "YOUR_SUPABASE_ANON_KEY"
+  "https://hpaaisyxnscherkrjnas.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwYWFpc3l4bnNjaGVya3JqbmFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MDg1MjAsImV4cCI6MjA5NTQ4NDUyMH0.UUgIxLsoL9HzN6BNulhe8m0F1hbo9BBEaYezLtypiho"
 );
