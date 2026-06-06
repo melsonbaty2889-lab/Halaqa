0131f07fad9cddf10d245d3ec6e402f1c1cf609a
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-06-07 02:16:35 +0300

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  "https://hpaaisyxnscherkrjnas.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwYWFpc3l4bnNjaGVya3JqbmFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MDg1MjAsImV4cCI6MjA5NTQ4NDUyMH0.UUgIxLsoL9HzN6BNulhe8m0F1hbo9BBEaYezLtypiho"
-);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
