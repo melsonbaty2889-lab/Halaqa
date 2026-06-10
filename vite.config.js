5f9f0a2878403e343ff4c3be5cab788863955a53
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-06-11 00:58:52 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,33 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  
-  // تحسين أداء البناء وتوزيع الملفات
   build: {
     outDir: 'dist',
-    sourcemap: false, // تعطل لتقليل حجم ملفات البناء وزيادة الأمان
-    rollupOptions: {
-      output: {
-        // تقسيم ملفات الـ JS الكبيرة لسرعة تحميل أكبر (Code Splitting)
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    },
-    // تقليل حجم ملفات الكود لسرعة أفضل
-    minify: 'terser',
   },
-  
-  // تحسين دقة المسارات لتجنب أخطاء النشر على Vercel
   resolve: {
     alias: {
       '@': '/src',
     },
-  },
-  
-  server: {
-    port: 3000,
   }
 })
