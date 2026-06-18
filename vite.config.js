50c11cc47854633a94fbd237e729f67b1be2823f
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-06-19 00:59:36 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,26 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
   resolve: {
     alias: {
-      '@': '/src',
+      '@': '/src', // المسار الذكي المعتمد لتسهيل استدعاء الملفات
     },
-  }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false, // إلغاء خرائط الكود في النسخة الحية لتسريع الـ Build وتقليص الحجم والأمان
+    chunkSizeWarningLimit: 1000, // رفع حد التحذيرات البرمجية للمكتبات الكبيرة مستقبلاً
+    rollupOptions: {
+      output: {
+        // الهندسة العكسية لتقسيم الكود (Code Splitting) لضمان سرعة صاروخية للموقع
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor-react'; // عزل مكتبة ريأكت الأساسية
+            if (id.includes('lucide') || id.includes('icons')) return 'vendor-icons'; // عزل الأيقونات
+            return 'vendor-libs'; // عزل باقي المكتبات وقاعدة البيانات تلقائياً
+          }
+        },
+      },
+    },
+  },
 })
