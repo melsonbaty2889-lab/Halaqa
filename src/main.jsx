e330cc55f5f5b208b50c7131a9cfa35f669c40be
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-06-19 02:20:32 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './i18n';
+import './index.css' // 🎨 حقن ملف التايلوند والتنسيقات العالمية في نقطة الإنطلاق الأساسية
+import './i18n'      // 🌐 تشغيل محرك اللغات الفوري وثنائي الاتجاه
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
