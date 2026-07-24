0d30e16affca0b44df5a5185566f167b035c5bf5
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-07-24 12:58:48 +0300

    Update sidebar component for mobile responsiveness

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -167,14 +167,14 @@ export function EnterpriseSidebar({
         />
       )}
 
-      {/* 2. السايدبار الرئيسي */}
+            {/* 2. السايدبار الرئيسي */}
       <aside 
         className={cn(
           "fixed top-0 bottom-0 z-50 h-screen w-72 bg-slate-900 text-slate-100 flex flex-col select-none",
           "transition-transform duration-300 ease-in-out border-slate-800",
           "rtl:right-0 ltr:left-0 rtl:border-l ltr:border-r",
           "lg:sticky lg:translate-x-0",
-          isOpenMobile ? "translate-x-0" : "rtl:translate-x-full ltr:-translate-x-full"
+          isOpenMobile ? "translate-x-0" : "max-lg:rtl:translate-x-full max-lg:ltr:-translate-x-full"
         )}
       >
         {/* الهيدر العلوي */}
