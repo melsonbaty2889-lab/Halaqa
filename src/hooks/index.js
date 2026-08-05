dcd565e07f1ac920795ce91ec4a7c091126840a4
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 01:19:23 +0300

    Add index.js to export custom hooks

---
+++ src/hooks/index.js
@@ -0,0 +1,4 @@
+/* src/hooks/index.js */
+export { default as useAttendance } from './useAttendance';
+export { default as useStudents } from './useStudents';
+export { default as useSubscription } from './useSubscription';
