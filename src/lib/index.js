ca6fe0cbb773e2d7a059853a1286361346e23261
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 01:22:38 +0300

    Create index.js to export multiple services

---
+++ src/lib/index.js
@@ -0,0 +1,6 @@
+/* src/lib/index.js */
+export * from './dashboardService';
+export * from './sessionService';
+export * from './supabase';
+export * from './uploadHelper';
+export * from './utils';
