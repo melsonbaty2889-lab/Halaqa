f577245aec31f40804e50994a026ead4d35b38d9
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 01:59:45 +0300

    Create Dashboard index file for exports

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,4 @@
+/* src/components/Dashboard/index.js */
+export { default as Dashboard } from './Dashboard';
+export { default as AcademyDashboard } from './AcademyDashboard';
+export { default as AdminDashboard } from './AdminDashboard';
