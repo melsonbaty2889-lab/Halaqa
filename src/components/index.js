6149e1168e4e8778493444bdc670e07e374d57b7
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 00:20:48 +0300

    Add index.js for component exports

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+// Student Module Exports
+export * from './Student';
+
+// Core Dashboard & Application Components
+export { default as AcademyDashboard } from './AcademyDashboard';
+export { default as AdminDashboard } from './AdminDashboard';
+export { default as Dashboard } from './Dashboard';
+export { default as MainApp } from './MainApp';
+export { default as Sidebar } from './Sidebar';
+
+// Features & Modules
+export { default as GamificationStreaks } from './GamificationStreaks';
+export { default as Attendance } from './Attendance';
+export { default as Teachers } from './Teachers';
+export { default as Reports } from './Reports';
+export { default as Payments } from './Payments';
