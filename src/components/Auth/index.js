00499734b374271910325a9341d41f3db78797c1
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 01:44:24 +0300

    Export authentication components from index.js

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,1 +1,5 @@
-
+/* src/components/Auth/index.js */
+export { default as LoginPage } from './LoginPage';
+export { default as SignUpPage } from './SignUpPage';
+export { default as ForgotPassword } from './ForgotPassword';
+export { default as UpdatePassword } from './UpdatePassword';
