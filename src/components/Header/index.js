ae9e8c90cf670afd1e371040c9bd695599c47a4e
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 21:10:53 +0300

    Delete src/components/Header/index.js

--- src/components/Header/index.js
+++
@@ -1,5 +0,0 @@
-/* src/components/Header/index.js */
-export { default as Header } from './Header';
-export { default as CurrencySelector } from './CurrencySelector';
-export { default as LanguageSwitcher } from './LanguageSwitcher';
-export { default as NotificationMenu } from './NotificationMenu';
