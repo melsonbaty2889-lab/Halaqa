20c59db8c341d9c972da0311763787ce1effd117
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 09:51:11 +0300

    Add QuranProgress module exports

---
+++ src/components/QuranProgress/index.js
@@ -0,0 +1,4 @@
+/* src/components/QuranProgress/index.js */
+export { default as QuranProgressBar } from './QuranProgressBar';
+export { default as QuranProgressSelector } from './QuranProgressSelector';
+export { default as HijriDate } from './HijriDate';
