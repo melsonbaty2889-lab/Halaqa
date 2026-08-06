a25eeedd680d1ca46f336facefd1ddea12b563d9
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 09:52:25 +0300

    Replace HijriDate export with DisplayDate export

--- src/components/QuranProgress/index.js
+++ src/components/QuranProgress/index.js
@@ -1,4 +1,4 @@
 /* src/components/QuranProgress/index.js */
 export { default as QuranProgressBar } from './QuranProgressBar';
 export { default as QuranProgressSelector } from './QuranProgressSelector';
-export { default as HijriDate } from './HijriDate';
+export { default as DisplayDate } from './DisplayDate';
