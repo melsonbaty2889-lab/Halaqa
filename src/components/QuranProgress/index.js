0e9b2c6244f674d562d3e117e3cbabf87c04fd34
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 21:11:29 +0300

    Delete src/components/QuranProgress/index.js

--- src/components/QuranProgress/index.js
+++
@@ -1,4 +0,0 @@
-/* src/components/QuranProgress/index.js */
-export { default as QuranProgressBar } from './QuranProgressBar';
-export { default as QuranProgressSelector } from './QuranProgressSelector';
-export { default as DisplayDate } from './DisplayDate';
