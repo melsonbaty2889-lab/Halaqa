69a36f94fbe6aa21bd5d4070740b37ba615dacc7
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 15:46:51 +0300

    Add index.js to export header components

---
+++ src/components/header/index.js
@@ -0,0 +1,5 @@
+/* src/components/Header/index.js */
+export { default as Header } from './Header';
+export { default as CurrencySelector } from './CurrencySelector';
+export { default as LanguageSwitcher } from './LanguageSwitcher';
+export { default as NotificationMenu } from './NotificationMenu';
