6b2189c57621ec37e3a72bc52b0eacb299cb775a
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 21:12:04 +0300

    Delete src/components/Student/index.js

--- src/components/Student/index.js
+++
@@ -1,4 +0,0 @@
-export { default as StudentBadges } from './StudentBadges';
-export { default as StudentProfile } from './StudentProfile';
-export { default as StudentStatsCard } from './StudentStatsCard';
-export { default as StudentsList } from './StudentsList';
