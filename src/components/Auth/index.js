b8e04e419acc4fe1f56aa92330d545ca81b3aad1
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 21:10:06 +0300

    Delete src/components/Auth/index.js

--- src/components/Auth/index.js
+++
@@ -1,5 +0,0 @@
-/* src/components/Auth/index.js */
-export { default as LoginPage } from './LoginPage';
-export { default as SignUpPage } from './SignUpPage';
-export { default as ForgotPassword } from './ForgotPassword';
-export { default as UpdatePassword } from './UpdatePassword';
