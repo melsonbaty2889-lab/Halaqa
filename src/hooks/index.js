68331ce595647f9b2b8a20c62b9dd28515309918
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 21:16:38 +0300

    Delete src/hooks/index.js

--- src/hooks/index.js
+++
@@ -1,4 +0,0 @@
-/* src/hooks/index.js */
-export { default as useAttendance } from './useAttendance';
-export { default as useStudents } from './useStudents';
-export { default as useSubscription } from './useSubscription';
