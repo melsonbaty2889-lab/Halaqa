5e7605f49af3d0950a8e757aa3b2796e232d3d3d
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 21:10:25 +0300

    Delete src/components/Dashboard/index.js

--- src/components/Dashboard/index.js
+++
@@ -1,4 +0,0 @@
-/* src/components/Dashboard/index.js */
-export { default as Dashboard } from './Dashboard';
-export { default as AcademyDashboard } from './AcademyDashboard';
-export { default as AdminDashboard } from './AdminDashboard';
