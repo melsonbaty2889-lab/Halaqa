e34e94e42a02009e58d3a589749d1762c12baaa6
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 21:16:00 +0300

    Delete src/utils/index.js

--- src/utils/index.js
+++
@@ -1,7 +0,0 @@
-/* src/utils/index.js */
-export * from './dateUtils';
-export * from './errorHandler';
-export * from './formatters';
-export * from './quranUtils';
-export * from './studentUtils';
-export * from './whatsappUtils';
