4e372f600ed66ff90fca637e8eb71732c6e3d7d4
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-06 21:09:50 +0300

    Delete src/components/UI/index.js

--- src/components/UI/index.js
+++
@@ -1,7 +0,0 @@
-/* src/components/UI/index.js */
-export { default as LoadingButton } from './LoadingButton';
-export { default as Skeleton } from './Skeleton';
-export { default as EmptyState } from './EmptyState';
-export { default as SmartHalaqaProLogo } from './SmartHalaqaProLogo';
-export { default as SplashScreen } from './SplashScreen';
-export { default as UI } from './UI';
