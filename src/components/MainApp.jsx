6ea9bb934e9ee92ef832ee8373089dd733ec2598
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-07 19:11:08 +0300

    Fix Header import path in MainApp component

--- src/components/MainApp.jsx
+++ src/components/MainApp.jsx
@@ -9,7 +9,7 @@ import { ROLES } from '@/constants/roles';
 
 // 🧩 استيراد المكونات المباشرة من أماكنها الصحيحة (حل مشكلة ./index)
 import Sidebar from '@/components/Sidebar';
-import Header from '@/components/Header';
+import Header from '@/components/Header/Header'; 
 import Dashboard from '@/components/Dashboard';
 import SubscriptionPage from '@/components/SaaS/SubscriptionPage';
 
