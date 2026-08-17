ebd4e5c21e2bd2a2711807e73ace71ca325a9eaf
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-17 19:43:40 +0300

    Refactor exports in theme index file

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,3 +1,3 @@
 // src/theme/index.js
-export { default as colors } from './colors';
-export { UI } from './styles';
+export { default as colors, colors } from './colors';
+export { default as UI, UI } from './styles';
