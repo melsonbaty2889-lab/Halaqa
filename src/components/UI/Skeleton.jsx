bba0c125a01668bb64ed8129db2dbd2ce7553504
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-17 10:36:36 +0300

    Update color import in Skeleton component

--- src/components/UI/Skeleton.jsx
+++ src/components/UI/Skeleton.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { colors as C } from '@/constants/colors';
+import colors from '@/theme/colors';
 
 // استخراج التأنيمشن ليعمل بشكل نظيف دون تكرار
 const SkeletonStyles = () => (
