8d05c73902e13b53e783b2d5c98284de13b0adda
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-22 00:43:14 +0300

    Replace HalaqaCard export with HalaqaCardItem

--- src/components/Halaqat/index.js
+++ src/components/Halaqat/index.js
@@ -1,3 +1,3 @@
 export { default as ActiveHalaqas } from './ActiveHalaqas.jsx';
 export { default as HalaqaFormModal } from './HalaqaFormModal.jsx';
-export { default as HalaqaCard } from './HalaqaCard.jsx';
+export { default as ​HalaqaCardItem } from './​HalaqaCardItem.jsx';
