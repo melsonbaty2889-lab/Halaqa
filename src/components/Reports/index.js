2ee029b2fe2aee2483cff6c2e99fbe31c61714b9
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-21 15:26:06 +0300

    Add exports for Reports components

---
+++ src/components/Reports/index.js
@@ -0,0 +1,4 @@
+export { default as Reports } from './Reports';
+export { default as ReportMetrics } from './ReportMetrics';
+export { default as StudentReportCard } from './StudentReportCard';
+export { default as TemplateSettings } from './TemplateSettings';
