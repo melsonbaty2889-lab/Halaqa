98acc31d3c2c4ff7707ebfdd8b62fc457ba8ac53
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-21 14:31:35 +0300

    Export Notification components from index.js

---
+++ src/components/Notifications/index.js
@@ -0,0 +1,3 @@
+export { default as NotificationCenter } from './NotificationCenter';
+export { default as NotificationTemplatesManager } from './NotificationTemplatesManager'; // اسم الملف المكتمل لديك
+export { default as CommunicationHub } from './CommunicationHub';
