8d0aba68969f1c5fc3abc284a6857a8349082bc3
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-21 12:31:59 +0300

    Export components from Teachers index file

---
+++ src/components/Teachers/index.js
@@ -0,0 +1,3 @@
+export { default as Teachers } from './Teacher'; 
+export { default as AddStaffModal } from './AddStaffModal';
+export { default as TeacherAvailabilityManager } from './TeacherAvailabilityManager';
