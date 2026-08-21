0e234a54d73590463828828c943451d6e4d38daa
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-22 01:16:35 +0300

    Delete src/components/Halaqat/index.js

--- src/components/Halaqat/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ActiveHalaqas } from './ActiveHalaqas.jsx';
-export { default as HalaqaFormModal } from './HalaqaFormModal.jsx';
-export { default as ​HalaqaCardItem } from './​HalaqaCardItem.jsx';
