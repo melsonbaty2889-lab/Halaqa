3fa99c16b9f81f4b377b5aef966fa043c49cab61
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-21 15:25:30 +0300

    Add index.js for UI component exports

---
+++ src/components/UI/index.js
@@ -0,0 +1,8 @@
+export { default as CustomDatePicker } from './CustomDatePicker';
+export { default as EmptyState } from './EmptyState';
+export { default as LoadingButton } from './LoadingButton';
+export { default as ReportDateSelector } from './ReportDateSelector';
+export { default as Skeleton } from './Skeleton';
+export { default as SmartHalaqaProLogo } from './SmartHalaqaProLogo';
+export { default as SplashScreen } from './SplashScreen';
+export { default as UI } from './UI';
