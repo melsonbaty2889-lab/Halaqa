03695508417a0f5c8e8e126958c7e65b3173bdda
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-22 10:27:55 +0300

    Add TeacherCard and TeacherFilter exports

--- src/components/Teachers/index.js
+++ src/components/Teachers/index.js
@@ -1,3 +1,5 @@
 export { default as Teachers } from './Teachers';
+export { default as TeacherCard } from './TeacherCard';
+export { default as TeacherFilter } from './TeacherFilter';
 export { default as AddStaffModal } from './AddStaffModal';
 export { default as TeacherAvailabilityManager } from './TeacherAvailabilityManager';
