758a0672d302bec8900f43d80fd28138dc969132
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-22 12:06:00 +0300

    Refactor exports in Teachers index file

--- src/components/Teachers/index.js
+++ src/components/Teachers/index.js
@@ -1,5 +1,3 @@
 export { default as Teachers } from './Teachers';
-export { default as TeacherCard } from './TeacherCard';
-export { default as TeacherFilter } from './TeacherFilter';
 export { default as AddStaffModal } from './AddStaffModal';
-export { default as TeacherAvailabilityManager } from './TeacherAvailabilityManager';
+export { default as TeacherAvailabilityManagement } from './TeacherAvailabilityManagement';
