0d43c10a388da116e58afc34b3ee735c0466245b
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-22 21:21:53 +0300

    Export student-related components from index.js

---
+++ src/components/Student/index.js
@@ -0,0 +1,7 @@
+export { default as AddStudentModal } from './AddStudentModal';
+export { default as StudentAttendanceCard } from './StudentAttendanceCard';
+export { default as StudentDocuments } from './StudentDocuments';
+export { default as StudentProfile } from './StudentProfile';
+export { default as StudentStatsCard } from './StudentStatsCard';
+export { default as StudentItemCard } from './StudentItemCard';
+export { default as StudentsList } from './StudentsList';
