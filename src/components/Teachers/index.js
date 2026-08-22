8b427f434fec006c967a57f24774401e221d9c03
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-22 10:14:22 +0300

    Fix export path for Teachers component

--- src/components/Teachers/index.js
+++ src/components/Teachers/index.js
@@ -1,3 +1,3 @@
-export { default as Teachers } from './Teacher'; 
+export { default as Teachers } from './Teachers';
 export { default as AddStaffModal } from './AddStaffModal';
 export { default as TeacherAvailabilityManager } from './TeacherAvailabilityManager';
