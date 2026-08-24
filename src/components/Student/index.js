ced2882c042d49943c062396aa336bf88a0e34a3
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-24 19:32:46 +0300

    Reorder and add exports in Student index file

--- src/components/Student/index.js
+++ src/components/Student/index.js
@@ -1,7 +1,7 @@
-export { default as StudentsList } from './StudentsList';
-export { default as StudentItemCard } from './StudentItemCard';
-export { default as StudentProfile } from './StudentProfile';
 export { default as AddStudentModal } from './AddStudentModal';
 export { default as StudentAttendanceCard } from './StudentAttendanceCard';
-export { default as StudentStatsCard } from './StudentStatsCard';
 export { default as StudentDocuments } from './StudentDocuments';
+export { default as DocumentUploadModal } from './DocumentUploadModal';
+export { default as StudentItemCard } from './StudentItemCard';
+export { default as StudentProfile } from './StudentProfile';
+export { default as StudentsList } from './StudentsList';
