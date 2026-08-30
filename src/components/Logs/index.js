ed761a64b0739416e9873f78d88484653498c4bf
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-08-30 09:28:17 +0300

    Export logs.config from Logs index

--- src/components/Logs/index.js
+++ src/components/Logs/index.js
@@ -1,2 +1,4 @@
+// src/components/Logs/index.js
 export { default as ActivityAuditLogs } from './ActivityAuditLogs';
 export { default as RealtimeAudit } from './RealtimeAudit';
+export * from './logs.config';
