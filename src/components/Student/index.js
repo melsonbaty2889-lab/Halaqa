6e3589474c8cc2191545493fa933522dd5a29751
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-09-02 11:09:20 +0300

    Update exports in Student index file

--- src/components/Student/index.js
+++ src/components/Student/index.js
@@ -1,7 +1,8 @@
 export { default as AddStudentModal } from './AddStudentModal';
+export { default as DocumentPreviewModal } from './DocumentPreviewModal';
+export { default as DocumentUploadModal } from './DocumentUploadModal';
 export { default as StudentAttendanceCard } from './StudentAttendanceCard';
 export { default as StudentDocuments } from './StudentDocuments';
-export { default as DocumentUploadModal } from './DocumentUploadModal';
 export { default as StudentItemCard } from './StudentItemCard';
 export { default as StudentProfile } from './StudentProfile';
 export { default as StudentsList } from './StudentsList';
