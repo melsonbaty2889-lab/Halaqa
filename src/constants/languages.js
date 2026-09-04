b5ff18c88397707f145c8b242d8e2fc504273280
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-09-05 01:59:50 +0300

    Add supported languages constant

---
+++ src/constants/languages.js
@@ -0,0 +1,9 @@
+/* src/constants/languages.js */
+export const SUPPORTED_LANGUAGES = [
+  { code: 'ar', name: 'العربية', nativeName: 'العربية', dir: 'rtl', flag: '🇸🇦' },
+  { code: 'en', name: 'الإنجليزية', nativeName: 'English', dir: 'ltr', flag: '🇬🇧' },
+  { code: 'fr', name: 'الفرنسية', nativeName: 'Français', dir: 'ltr', flag: '🇫🇷' },
+  { code: 'ur', name: 'الأوردو', nativeName: 'أردو', dir: 'rtl', flag: '🇵🇰' },
+  { code: 'tr', name: 'التركية', nativeName: 'Türkçe', dir: 'ltr', flag: '🇹🇷' },
+  { code: 'id', name: 'الأندونيسية', nativeName: 'Bahasa Indonesia', dir: 'ltr', flag: '🇮🇩' },
+];
