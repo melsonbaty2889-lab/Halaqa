e7d4074fac852922290aad2f78a8e0f454af5d28
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-09-07 20:00:41 +0300

    Update exports in index.js for UI components
    Removed export of UI component and added export for TermsModal.

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,8 +1,31 @@
+// تصدير مكونات UI الأساسية والمجمعة من UI.jsx
+export { 
+  Badge, 
+  Btn, 
+  Button, 
+  Card, 
+  Input, 
+  Select, 
+  Modal, 
+  PageHeader, 
+  Table, 
+  THead, 
+  TBody, 
+  TR, 
+  TH, 
+  TD 
+} from './UI';
+
+// تصدير باقي المكونات المستقلة داخل مجلد UI
+export { default as BlockedView } from './BlockedView';
+export { default as ConfirmModal } from './ConfirmModal';
+export { default as CountrySelect } from './CountrySelect';
 export { default as CustomDatePicker } from './CustomDatePicker';
+export { default as CustomSelect } from './CustomSelect';
 export { default as EmptyState } from './EmptyState';
 export { default as LoadingButton } from './LoadingButton';
 export { default as ReportDateSelector } from './ReportDateSelector';
 export { default as Skeleton } from './Skeleton';
 export { default as SmartHalaqaProLogo } from './SmartHalaqaProLogo';
 export { default as SplashScreen } from './SplashScreen';
-export { default as UI } from './UI';
+export { default as TermsModal } from './TermsModal';
