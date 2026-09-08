e7f277ca031ec1a77d3e1e8efd9f27b3753d06bc
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-09-08 14:57:23 +0300

    Update index.js

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -26,6 +26,7 @@ export { default as EmptyState } from './EmptyState';
 export { default as LoadingButton } from './LoadingButton';
 export { default as ReportDateSelector } from './ReportDateSelector';
 export { default as Skeleton } from './Skeleton';
+export { default as SelectModal } from './SelectModal';
 export { default as SmartHalaqaProLogo } from './SmartHalaqaProLogo';
 export { default as SplashScreen } from './SplashScreen';
 export { default as TermsModal } from './TermsModal';
