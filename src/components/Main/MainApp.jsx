04b505866a6c7c7823c84119fefdb8f1f4f2f4ea
melsonbaty2889-lab <m.elsonbaty2889@gmail.com>
2026-09-17 01:56:04 +0300

    Remove OfflineAndUpdateBanner import from MainApp
    Removed import of OfflineAndUpdateBanner component.

--- src/components/Main/MainApp.jsx
+++ src/components/Main/MainApp.jsx
@@ -10,7 +10,6 @@ import { UI } from '@/theme/styles.js';
 import { PageSkeleton } from '@/components/UI/Skeleton';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import BottomNav from '@/components/Sidebar/BottomNav';
-import OfflineAndUpdateBanner from '@/components/AppLayout/OfflineAndUpdateBanner';
 import Header from '@/components/Header/Header'; 
 import Dashboard from '@/components/Dashboard/Dashboard';
 import SubscriptionPage from '@/components/SaaS/SubscriptionPage';
